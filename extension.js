497f365cf195307b7dec6eb1b453399660e7621b
annaquainliu <annaquainliu@gmail.com>
2023-04-19 00:05:05 -0400

    edited config

--- extension.js
+++ extension.js
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const fs = require('fs');
+const path = require('path');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -17,11 +18,36 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('c---unit-tests.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('c---unit-tests.createBoilerplate', async function () {
 		// The code you place here will be executed every time your command is executed
+		if (!vscode.workspace) {
+			return vscode.window.showErrorMessage('Please open a project folder first');
+		  }
+		  
+		  const folderPath = vscode.workspace.workspaceFolders[0].uri
+			.toString()
+			.split(':')[1];
+		
+			const htmlContent = `<!DOCTYPE html>
+				<html lang="en">
+				<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+				<title>Document</title>
+				<link rel="stylesheet" href="app.css" />
+				</head>
+				<body>
+				<script src="app.js"></script>
+				</body>
+				</html>`;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from C++ Unit Tests!');
+			fs.writeFile(path.join(folderPath, 'index.html'), htmlContent, (err) => {
+				if (err) {
+					return vscode.window.showErrorMessage('Failed to create boilerplate file!');
+				}
+				vscode.window.showInformationMessage('Created boilerplate files');
+			});
 	});
 
 	context.subscriptions.push(disposable);
